a344d53dfb79048096177475dc547635ef99c754
YutakaAihara <youguisufanyuan@gmail.com>
2021-04-23 00:49:43 +0000

    [Update]エラー表示追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,11 +10,12 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book)
     else
+      @books = Book.all
       render action: :index
     end
   end
@@ -25,10 +26,10 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book)
+      redirect_to book_path(@book)
     else
       render action: :edit
     end
